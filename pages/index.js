c5900598ed103b98d163de2985933966869536e1
Tiago Murilo <tiago.murilo08@gmail.com>
2021-01-27 21:09:51 -0300

    Adjust for a new model page

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,10 @@ function Index(){
         <div className={styles.bodyAuthorize}>
             <img src='https://avatars.githubusercontent.com/u/62064267?s=460&u=47dd36f865b2c5023a74d21bc505abbcceec872e&v=4'/>
             <h2>Tiago Murilo Ochôa da Luz</h2>
-            <p>Full Stack Developer</p>
-            <h1>Welcome!<br/>To access my resume, enter the token below :</h1>
-            <form action="post">
-                <input type="text" placeholder="Insert the token"/>
-                <Link href='/requestAccess'><a><p>or request access by clicking here</p></a></Link>
-                <button type="submit">Access</button>
-            </form>
+            <p>Full Stack Developer</p>            
+            <div>
+                
+            </div>
         </div>
     )
 }
